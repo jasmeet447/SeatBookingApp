a82696de7d6462ba55f9da5a9bff1de9fe7362f9
jasmeet.singh@Pentair.com <jasmeet.singh@Pentair.com>
2022-10-13 14:38:33 +0530

    Create project

---
+++ ios/Pods/Target Support Files/Pods-newProj-newProjTests/Pods-newProj-newProjTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_newProj_newProjTests : NSObject
+@end
+@implementation PodsDummy_Pods_newProj_newProjTests
+@end
---
+++ ios/Pods/Target Support Files/Pods-newProj/Pods-newProj-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_newProj : NSObject
+@end
+@implementation PodsDummy_Pods_newProj
+@end
